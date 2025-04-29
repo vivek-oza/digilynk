0459a71a04624b765659b9c7973650fa5e799896
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2025-04-29 14:40:02 +0530

    Fix

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(helmet()); // Add security headers
 app.use(
     cors({
         origin:
-            import.meta.env.MODE === "development"
+            process.env.MODE === "development"
                 ? "http://localhost:5173"
                 : "https://digilynk.vercel.app",
         credentials: true,
