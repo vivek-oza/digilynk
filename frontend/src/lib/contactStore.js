ce4863e7575ad7ac44073fea9907ad73e0e12323
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2025-04-29 16:59:17 +0530

    Debug

--- frontend/src/lib/contactStore.js
+++ frontend/src/lib/contactStore.js
@@ -1,7 +1,9 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+axios.defaults.withCredentials = true;
+
+const API_BASE_URL = process.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 // const API_BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : import.meta.env.VITE_API_BASE_URL;
 
