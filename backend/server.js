960cb04f626971a638ba3f560a4e67fce645c01e
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2025-04-29 13:44:10 +0530

    minor fix 1

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,19 @@ const app = express();
 
 // Middlewares
 app.use(helmet()); // Add security headers
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true,
+// }));
+app.use(
+    cors({
+        origin:
+            process.env.MODE === "development"
+                ? "http://localhost:5173"
+                : "https://digilynk.vercel.app",
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 // Rate limiter to prevent spam
