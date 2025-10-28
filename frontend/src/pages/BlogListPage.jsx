b671230d0b72862291eaad9eabae0605ee560916
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2025-10-28 06:57:33 +0530

    contact and blog update

--- frontend/src/pages/BlogListPage.jsx
+++ frontend/src/pages/BlogListPage.jsx
@@ -112,7 +112,7 @@ export default function BlogListPage() {
           <div
             className="absolute inset-0 bg-cover bg-center bg-no-repeat"
             style={{
-              backgroundImage: `url('https://images.unsplash.com/photo-1673692011577-b98b96b0ed3c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170')`,
+              backgroundImage: `url('https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg')`,
             }}
           />
 
